fcee483633da0c28470b090cabf4837ce5c85830
KentKong123 <kongdehui157@gmail.com>
2023-06-14 21:48:51 +0800

    fix:在windows10专业版本下面antd4.20.5版本报错以及更新最近antd5.6.1版本后的错误
    错误1：在windows10专业版本下面antd4.20.5版本报错：Failed to parse source map: 'webpack://antd/./components/config-provider/style/index.less' URL is not supported
    错误2：使用npm更新最新版本antd5.6.1后，报错ERROR in ./src/index.js 7:0-28
    Module not found: Error: Can't resolve 'antd/dist/antd.css'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.js.map';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
